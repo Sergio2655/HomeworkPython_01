8994ad30b377d46ea43e25ad615561a59c853a64
Sergey <sp27101971@gmail.com>
2022-09-27 18:51:38 +0300

    FindCoordinates-f

---
+++ FindCoordinates.py
@@ -0,0 +1,14 @@
+# Напишите программу, которая по заданному номеру четверти, 
+# показывает диапазон возможных координат точек в этой четверти (x и y).
+
+quarter = int(input('Enter number of quarter: '))
+if quarter == 1:
+    print('x > 0 and y > 0')
+elif quarter == 2:
+    print('x > 0 and y < 0')
+elif quarter == 3:
+    print('x < 0 and y < 0')
+elif quarter == 4:
+    print('x < 0 and y > 0')
+else:
+    print('Wrong number')
